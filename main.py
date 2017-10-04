d6f9d14d857cd7bd751f6cfd9a892b5ee0b939d6
Natte Sharan <nattesharan@gmail.com>
2017-10-04 14:36:46 +0530

    Generated random text

--- main.py
+++ main.py
@@ -41,8 +41,27 @@ import nltk
 # print(chatroom)
 # print(nltk.corpus.brown.categories())
 # print(nltk.corpus.brown.words(categories='mystery'))
-fic = nltk.corpus.brown.words(categories='fiction')
-fdist = nltk.FreqDist([w.lower() for w in fic])
-m = ['what','when','where','who','why']
-for i in m:
-    print i + ':', fdist[i]
+# fic = nltk.corpus.brown.words(categories='fiction')
+# fdist = nltk.FreqDist([w.lower() for w in fic])
+# m = ['what','when','where','who','why']
+# for i in m:
+#     print i + ':', fdist[i]
+# print(nltk.corpus.reuters.fileids())
+# print(nltk.corpus.reuters.words('training/9964'))
+# print(nltk.corpus.reuters.categories())
+# print(nltk.corpus.reuters.categories('training/9978'))
+# print(nltk.corpus.inaugural.fileids())
+# print(nltk.corpus.inaugural.words('1993-Clinton.txt'))
+# print [i[:4] for i in nltk.corpus.inaugural.fileids()]
+'''Generating random text: This program obtains all bigrams from the text of the book
+of Genesis, then constructs a conditional frequency distribution to record which words are most likely
+to follow a given word; e.g., after the word living, the most likely word is creature; the
+generate_model() function uses this data, and a seed word, to generate random text.'''
+def generate_model(cfdist,word,num=15):
+    for i in range(num):
+        print word
+        word = cfdist[word].max()
+text = nltk.corpus.genesis.words('english-kjv.txt')
+bigrams = nltk.bigrams(text)
+cfd = nltk.ConditionalFreqDist(bigrams)
+generate_model(cfd, 'living')
