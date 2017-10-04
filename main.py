f520a68a8f86c3d47b09092acafe6f0cdfda5193
Natte Sharan <nattesharan@gmail.com>
2017-10-04 11:52:01 +0530

    gutenberg,webtext,chat sessions and brown corpus working

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # from nltk.book import *
 import nltk
-from nltk.book import *
 # text1.concordance('name')
 # text1.similar('love')
 # text2.common_contexts(['monstrous','very'])
@@ -24,8 +23,26 @@ from nltk.book import *
 # lng = [w for w in set(text5) if len(w)>7 and fdist[w]>7]
 # print(sorted(lng))
 # print(nltk.corpus.gutenberg.fileids())
-emma = nltk.Text(nltk.corpus.gutenberg.words('austen-emma.txt'))
+# emma = nltk.Text(nltk.corpus.gutenberg.words('austen-emma.txt'))
 # print(len(emma))
 # fdist = FreqDist(emma)
 # fdist.plot(50,cumulative=True)
 # emma.dispersion_plot(['surprize'])
+# for geting the number of senteces
+# emma_sents = nltk.corpus.gutenberg.sents('austen-emma.txt')
+# print(len(emma_sents))
+# byrant_sents = nltk.corpus.gutenberg.sents('bryant-stories.txt')
+# print(len(byrant_sents))
+# working with web text
+# print(nltk.corpus.webtext.fileids())
+# for webtext in nltk.corpus.webtext.fileids():
+#     print webtext,nltk.corpus.webtext.raw(webtext)
+# chatroom = nltk.corpus.nps_chat.fileids()
+# print(chatroom)
+# print(nltk.corpus.brown.categories())
+# print(nltk.corpus.brown.words(categories='mystery'))
+fic = nltk.corpus.brown.words(categories='fiction')
+fdist = nltk.FreqDist([w.lower() for w in fic])
+m = ['what','when','where','who','why']
+for i in m:
+    print i + ':', fdist[i]
